9c24224c160d1611cbf170dad41c848c8618daba
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-10 09:29:14 +0330

    js part1

---
+++ script.js
@@ -0,0 +1,22 @@
+const startBtn = document.querySelector(".start"),
+  stopBtn = document.querySelector(".stop"),
+  resetBtn = document.querySelector(".reset");
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
+resetBtn.addEventListener("click", reset);
+
+function start() {
+  startBtn.classList.add("active");
+  stopBtn.classList.remove("stopActive");
+}
+
+function stop() {
+  startBtn.classList.remove("active");
+  stopBtn.classList.add("stopActive");
+}
+
+function reset() {
+  startBtn.classList.remove("active");
+  stopBtn.classList.remove("stopActive");
+}
